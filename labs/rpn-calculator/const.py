ea8097782d82d0fb4453387ffd3afd234976856b
Ethan <ethan.hawk@valpo.edu>
2021-10-04 14:01:51 -0500

    frontend.py & const.py: refactor frontend.py into functions

---
+++ labs/rpn-calculator/const.py
@@ -0,0 +1,12 @@
+
+ast_NUM = 1
+ast_OP  = 2
+
+
+
+ins_PUSH = 0
+ins_ADD = 1
+ins_MUL = 2
+ins_SUB = 3
+ins_DIV = 4
+
--- labs/rpn-calculator/frontend.py
+++ labs/rpn-calculator/frontend.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python
 
-filename="test.rpn"
+import const
 
-ast_NUM = 1
-ast_OP  = 2
+def get_ast_from_f(filename):
+    ast_list = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.replace('\n', '')
+            print(line)
+            for tok in line.split(' '):
+                print(tok)
+                if tok.isdigit():
+                    ast_list.append((const.ast_NUM, tok))
+                if "+-*/".__contains__(tok):
+                    ast_list.append((const.ast_OP, tok))
+    return ast_list
 
-ast_list = []
+ast_list = get_ast_from_f("test.rpn")
 
-with open(filename) as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.replace('\n', '')
-        print(line)
-        for tok in line.split(' '):
-            print(tok)
-            if tok.isdigit():
-                ast_list.append((ast_NUM, tok))
-            if "+-*/".__contains__(tok):
-                ast_list.append((ast_OP, tok))
+def get_ins_from_ast(ast_list):
+    ins_list = []
+    for ast_node in ast_list:
+        ast_type, ast_val = ast_node
+        if ast_type == const.ast_NUM:
+            ins_list.append((const.ins_PUSH, ast_val))
+        if ast_type == const.ast_OP:
+            if ast_val == "+":
+                ins_list.append((const.ins_ADD, ast_val))
+            if ast_val == "-":
+                ins_list.append((const.ins_SUB, ast_val))
+            if ast_val == "*":
+                ins_list.append((const.ins_MUL, ast_val))
+            if ast_val == "/":
+                ins_list.append((const.ins_DIV, ast_val))
+    return ins_list
 
-print(ast_list)
-
-ins_PUSH = 0
-ins_ADD = 1
-ins_MUL = 2
-ins_SUB = 3
-ins_DIV = 4
-
-ins_list = []
-
-for ast_node in ast_list:
-    ast_type, ast_val = ast_node
-    if ast_type == ast_NUM:
-        ins_list.append((ins_PUSH, ast_val))
-    if ast_type == ast_OP:
-        if ast_val == "+":
-            ins_list.append((ins_ADD, ast_val))
-        if ast_val == "-":
-            ins_list.append((ins_SUB, ast_val))
-        if ast_val == "*":
-            ins_list.append((ins_MUL, ast_val))
-        if ast_val == "/":
-            ins_list.append((ins_DIV, ast_val))
-
-print(ins_list)
-
+print(get_ins_from_ast(ast_list))
