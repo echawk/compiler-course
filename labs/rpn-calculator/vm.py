f70a0864e3ae5222287fdaae6ae4e59a08f3b778
Ethan <ethan.hawk@valpo.edu>
2021-10-05 17:12:59 -0500

    vm.py & test.rpn: fix polish notation

--- labs/rpn-calculator/vm.py
+++ labs/rpn-calculator/vm.py
@@ -8,25 +8,21 @@ def vm(program):
         if ins == const.ins_PUSH:
             stack.append(value)
         if ins == const.ins_ADD:
-            while len(stack) > 1:
-                right = stack.pop()
-                left  = stack.pop()
-                stack.append(left + right)
+            right = stack.pop()
+            left  = stack.pop()
+            stack.append(left + right)
         if ins == const.ins_SUB:
-            while len(stack) > 1:
-                right = stack.pop()
-                left  = stack.pop()
-                stack.append(left - right)
+            right = stack.pop()
+            left  = stack.pop()
+            stack.append(left - right)
         if ins == const.ins_MUL:
-            while len(stack) > 1:
-                right = stack.pop()
-                left  = stack.pop()
-                stack.append(left * right)
+            right = stack.pop()
+            left  = stack.pop()
+            stack.append(left * right)
         if ins == const.ins_DIV:
-            while len(stack) > 1:
-                right = stack.pop()
-                left  = stack.pop()
-                stack.append(left // right)
+            right = stack.pop()
+            left  = stack.pop()
+            stack.append(left // right)
         prog_counter += 1
         print(stack)
     print(stack)
