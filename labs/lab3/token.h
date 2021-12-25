e67740987310aa225ee685e18c0d4f872e3d8e6c
Ethan <ethan.hawk@valpo.edu>
2021-12-24 20:05:42 -0600

    lab3: added an example of lex

---
+++ labs/lab3/token.h
@@ -0,0 +1,21 @@
+/*
+  Here we are creating an enum 'tok' that stores the internal values
+  for our different token types
+*/
+enum tok{
+	IF,
+	LET,
+	IDENT,
+	FUNC,
+	ASSIGN,
+	INT_LIT,
+	SEMI,
+	PLUS,
+	MINUS,
+	ASTERISK,
+	DIV,
+	LPAREN,
+	RPAREN,
+	ILLEGAL
+};
+
